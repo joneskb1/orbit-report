4bb747221e0b9cd06e0e04e8b17cb5b395975ad7
Kayla Jones <kayla3445@yahoo.com>
2020-12-12 14:25:00 -0600

    working on step 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Satellite } from "./satellite";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'orbit-report';
+  title = "orbit-report";
+  sourceList: Satellite[];
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl =
+      "https://handlers.education.launchcode.org/static/satellites.json";
+
+    window.fetch(satellitesUrl).then(
+      function (response) {
+        response.json().then(
+          function (data) {
+            let fetchedSatellites = data.satellites;
+            for (let i = 0; i < fetchedSatellites.length; i++) {
+              let newSat = new Satellite(
+                fetchedSatellites[i].name,
+                fetchedSatellites[i].type,
+                fetchedSatellites[i].launchDate,
+                fetchedSatellites[i].orbitType,
+                fetchedSatellites[i].operational
+              );
+              this.sourceList.push(newSat);
+            }
+          }.bind(this)
+        );
+      }.bind(this)
+    );
+  }
 }
